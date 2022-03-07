00d5f8f05971cc6b4be66f788ab8548fd819dbc3
Sera <seramokeira@gmail.com>
2022-03-07 08:06:26 +0300

    bug ;add form variables

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+const days ={Sunday,Monday,Tuesday,Wednesday,Thursday,Friday};
+
+const Female=days.map(Akosua,Adwoa,Abenaa,Akua,Yaa,Afua,Ama)
+console.log(female);
+
+const Male={ Kwasi,Kwadwo,Kwabena,Kwaku,Yaw,Kofi,Kwame}
